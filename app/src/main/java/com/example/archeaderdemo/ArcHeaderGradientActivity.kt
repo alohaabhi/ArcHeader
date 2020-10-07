ac233df99d1b593367ffa23826b7ffabbb4ec1ea
iabhishek1041 <iabhishek1041@gmail.com>
2020-10-07 13:27:56 +0530

    Add separate activities for differet types

---
+++ app/src/main/java/com/example/archeaderdemo/ArcHeaderGradientActivity.kt
@@ -0,0 +1,12 @@
+package com.example.archeaderdemo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ArcHeaderGradientActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_arc_header_gradient)
+    }
+}
--- app/src/main/java/com/example/archeaderdemo/MainActivity.kt
+++ app/src/main/java/com/example/archeaderdemo/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.archeaderdemo
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +10,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btn_open_arc_header).setOnClickListener {
+            startActivity(Intent(this, ArcHeaderActivity::class.java))
+        }
+
+        findViewById<Button>(R.id.btn_open_arc_header_gradient).setOnClickListener {
+            startActivity(Intent(this, ArcHeaderGradientActivity::class.java))
+        }
     }
 }
